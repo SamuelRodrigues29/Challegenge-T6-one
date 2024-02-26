86074d046fae2a4bc66e4b19f367e0c8d0fb971a
SamuelRodrigues29 <141246622+SamuelRodrigues29@users.noreply.github.com>
2024-02-26 19:38:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const textoInput = document.getElementById('input-texto');
+
+const outInput = document.querySelector("output");
+
+
+  function criptografar(){
+    let texto = textoInput.value;
+
+    let cripto = texto
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+    document.getElementById("output").innerHTML = '<textarea readonly id = "input-texto'> + cripto + 
+    '</textarea>' + '<button class= "btn-copiar" id="copiar" onclick = "copiar()">Copiar</button>'  
+    add.EventListener()  
+}
+
+ function descriptografar(){
+    let texto = textoInput.value;
+    
+    let descripto = texto
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u")
+    document.getElementById("output").innerHTML = '<textarea readonly id = "input-text'> + descripto + 
+    '</textarea>' + '<button class= "btn-copiar" onclick = "copiar()">Copiar</button>'
+    add.EventListener() 
+}   
+
+function copiar() {
+    var textoCop = document.getElementById("#input-texto");
+    textoCop.select();
+    document.execCommand('copy');
+}
+
+   
+   
